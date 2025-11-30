b8674fae15fa629d8d4e72d6e862ebb18071d9ac
FidoInformatichien <38144752+cerede2000@users.noreply.github.com>
2025-11-30 01:32:13 +0100

    Update main.go

--- src/main.go
+++ src/main.go
@@ -11,7 +11,6 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/events"
 	"github.com/docker/docker/api/types/filters"
